80a2da5a026b83f355d39f032e249f00dfbb7802
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-06-02 17:01:54 -0400

    corrected indentation on api.js.

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,8 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
+  Transaction.find({})
+    .sort({date: -1})
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
